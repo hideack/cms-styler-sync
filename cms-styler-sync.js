17914f3d94f7639d93ba5fc5884c541bffd54406
Hideaki Terai <hideack@pepabo.com>
2023-11-03 20:55:18 +0900

    テンプレートのIDの指定は任意にした

--- cms-styler-sync.js
+++ cms-styler-sync.js
@@ -1,24 +1,31 @@
 const program = require('commander');
-const { fetchData, uploadTemplates, fetchTemplate } = require('./shopProScraper');
+const { fetchData, uploadTemplates, fetchTemplate, fetchDefaultTemplateUid } = require('./shopProScraper');
 
 program
   .version('1.0.0')
   .option('-i, --id <id>', 'Login ID')
   .option('-p, --password <password>', 'Login Password')
-  .option('-u, --uid <uid>', 'Template UID')
+  .option('-u, --uid [uid]', 'Template UID')
   .option('--import', 'Import templates from admin.shop-pro.jp')
   .option('--export', 'Export local templates to admin.shop-pro.jp')
-  .action((options) => {
-    if (!options.id || !options.password || !options.uid) {
-      console.error('ID, PASSWORD, and UID are all required.');
+  .action(async (options) => {
+    if (!options.id || !options.password) {
+      console.error('ID and PASSWORD are required.');
       program.help();
       process.exit(1);
     }
 
+    let tmplUid = options.uid;
+    if (!tmplUid) {
+      console.log('UID not provided. Fetching default template UID...');
+      tmplUid = await fetchDefaultTemplateUid(options.id, options.password);
+      console.log(`Fetched UID: ${tmplUid}`);
+    }
+
     if (options.import) {
-      fetchTemplate(options.id, options.password, options.uid);
+      fetchTemplate(options.id, options.password, tmplUid);
     } else if (options.export) {
-      uploadTemplates(options.id, options.password, options.uid);
+      uploadTemplates(options.id, options.password, tmplUid);
     } else {
       console.error('Either --import or --export must be specified.');
       program.help();
